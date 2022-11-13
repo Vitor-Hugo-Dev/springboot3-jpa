e44e4f4ad43b6cb2aef5893ad4a20f5a23a66383
Vitor Hugo <vitorhugo.dev1997@gmail.com>
2022-11-13 19:22:01 -0300

    Order, Instant, ISO 8601: parte 1

---
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -0,0 +1,61 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "table_order")
+public class Order implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue()
+  private Long id;
+
+  private Instant moment;
+
+  @ManyToOne
+  @JoinColumn(name = "client_id")
+  private User client;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Instant getMoment() {
+    return moment;
+  }
+
+  public void setMoment(Instant moment) {
+    this.moment = moment;
+  }
+
+  public User getClient() {
+    return client;
+  }
+
+  public void setClient(User client) {
+    this.client = client;
+  }
+
+  public Order(Long id, Instant moment, User client) {
+    this.id = id;
+    this.moment = moment;
+    this.client = client;
+  }
+
+  public Order() {
+  }
+}
--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,11 +1,14 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -13,6 +16,7 @@ import jakarta.persistence.Table;
 public class User implements Serializable {
 
   private static final long serialVersionUID = 1L;
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -20,6 +24,13 @@ public class User implements Serializable {
   private String email;
   private String phone;
   private String password;
+
+  @OneToMany(mappedBy = "client")
+  private List<Order> orders = new ArrayList<>();
+
+  public List<Order> getOrders() {
+    return orders;
+  }
 
   public Long getId() {
     return id;
